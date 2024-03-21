5a9bbfacbee93e46f409a0d1bdbb77990f95aa45
barbod garshas <barbodgarshasbi79@gmail.com>
2024-03-21 20:27:05 +0100

    -

--- Portfolio/src/App.vue
+++ Portfolio/src/App.vue
@@ -1,7 +1,16 @@
 <script setup>
 
 import TheHeader from '@/components/TheHeader.vue'
+import PostCard from '@/components/ProjectCard.vue'
 
+import projectPicWordle from '@/assets/Projects/wordle-screenshot.png'
+import projectPicCafeLotte from '@/assets/Projects/Cafe Lotte Screenshot.png'
+import projectPicCurrency from '@/assets/Projects/Curency-Converter.png'
+import projectPicDDC from '@/assets/Projects/DDC Remake Screenshot.png'
+import projectPicEplodedView from '@/assets/Projects/Exploded View Screenshot.png'
+import projectPicGalleryVJ from '@/assets/Projects/Gallery Website homepage Screeshot.png'
+import projectPicGalleryReact from '@/assets/Projects/Gallery Website Screeshot.png'
+import projectPicTODOs from '@/assets/Projects/TODOs Screenshot.png'
 </script>
 
 <template>
@@ -17,8 +26,31 @@ import TheHeader from '@/components/TheHeader.vue'
           <p class="hero__title">My workflow & Approach</p>
           <p class="hero__text">My Workflow differs from project to project but what stays the same is my approach. I try to understand the project and its needs and while doing so I determine the best tools to built it. <br><br> For one project I might go with Test Driven Development, Scrum Board, React, and Type Script and for another just a simple sketch on a paper and vanilla JS.</p>
         </div>
+        <div class="hero__action">
+          <div class="hero__btn">
+            <p class="hero__btn__p">To The Projects</p>
+          </div>
+        </div>
       </div>
     </div>
+
+    <div class="projects">
+      <div class="projects__row">
+        <PostCard :image="projectPicWordle" title="Wordle Game with Vue"></PostCard>
+        <PostCard :image="projectPicCafeLotte" title="Cafe lotte"></PostCard>
+        <PostCard :image="projectPicDDC" title="Dance School Website"></PostCard>
+      </div>
+      <div class="projects__row">
+        <PostCard :image="projectPicEplodedView" title="Portfolio with Exploded View"></PostCard>
+        <PostCard :image="projectPicCurrency" title="Currency Converter with API + Dark Mode"></PostCard>
+        <PostCard :image="projectPicTODOs" title="TODOs App with Vanila JS"></PostCard>
+      </div>
+      <div class="projects__row">
+        <PostCard :image="projectPicGalleryVJ" title="Gallery Website"></PostCard>
+        <PostCard :image="projectPicGalleryReact" title="Gallery Website with React"></PostCard>
+      </div>
+    </div>
+    
   </main>
 </template>
 
@@ -31,6 +63,14 @@ import TheHeader from '@/components/TheHeader.vue'
     box-sizing: border-box;
     
     font-family: 'Poppins', sans-serif;
+  }
+  ::-webkit-scrollbar {
+    width: 0;  /* Remove scrollbar space */
+    background: transparent;  /* Optional: just make scrollbar invisible */
+  }
+  /* Optional: show position indicator in red */
+  ::-webkit-scrollbar-thumb {
+      background: #FF0000;
   }
 
   *, *:before, *:after {
@@ -70,7 +110,7 @@ import TheHeader from '@/components/TheHeader.vue'
     overflow: hidden;
     position: relative;
     width: 100%;
-    height: 80vh;
+    height: 88vh;
     .hero__image {
       position: absolute;
       width: 100%;
@@ -84,12 +124,15 @@ import TheHeader from '@/components/TheHeader.vue'
       position: relative;
       background: linear-gradient(0deg, rgba(25,25,25,1) 0%, rgba(25,25,25,.7) 15%, rgba(25,25,25,.7) 99%, rgba(25,25,25,1) 100%);
 
+      display: flex;
+
       width: 100%;
       height: 100%;
       z-index: 9998;
 
       .hero__info {
         position: relative;
+
         width: 50%;
         height: 100%;
         padding: 4rem;
@@ -112,7 +155,51 @@ import TheHeader from '@/components/TheHeader.vue'
 
         }
       }
+
+      .hero__action {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        width: 50%;
+        .hero__btn {
+          cursor: pointer;
+
+          background-color: #D65A31;
+          box-shadow: 0px 0px 20px 2px #D65A31;
+          border-radius: 15rem;
+          padding: 2rem 4rem;
+
+          transition-duration: 100ms;
+          transition-timing-function: ease-in;
+          .hero__btn__p {
+            color: #191919;
+            font-size: 2rem;
+            font-weight: 500;
+            transition-duration: 100ms;
+            transition-timing-function: ease-in;
+          }
+        }
+        .hero__btn:hover {
+          box-shadow: 0px 0px 30px 3px #D65A31;
+          .hero__btn__p {
+            color: #EEEEEE;
+          }
+        }
+      }
     }
-}
+  }
+
+  .projects {
+    display: flex;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+    .projects__row {  
+      padding: 1rem 0;
+      display: flex;
+      justify-content: space-evenly;
+      flex-wrap: wrap;
+    }
+  }
 
 </style>
---
+++ Portfolio/src/components/ProjectCard.vue
@@ -0,0 +1,61 @@
+<script setup>
+
+const props = defineProps({
+    title: {
+        default: '',
+        type: String
+    },
+    image: {
+        default: null,
+        type: Image
+    }
+})
+
+</script>
+
+<template>
+    <div class="card">
+        <img :src="props.image" alt="background image" class="card__background">
+        <p class="card__title">{{ props.title }}</p>
+    </div>
+</template>
+
+<style lang="scss">
+    .card {
+        cursor: pointer;
+        position: relative;
+        overflow: hidden;
+        width: 30%;
+        height: 30vh;
+        border-radius: 12px;
+
+
+        .card__background {
+            width: 100%;
+            border-radius: 12px;
+            
+        }
+
+        .card__title {
+            box-sizing: border-box;
+            color: #EEEEEE;
+            background: linear-gradient(0deg, rgba(25,25,25,1) 0%, rgba(25,25,25,0) 100%);
+            position: absolute;
+            bottom: 0;
+            left: 0;
+
+            display: flex;
+            align-items: flex-end;
+            padding: 1.5rem 1.2rem;            
+
+            width: 100%;
+            height: 100%;
+
+            font-size: 1.5rem;
+            font-weight: 500;
+            transition-duration: 300ms;
+
+            
+        }
+    }
+</style>
